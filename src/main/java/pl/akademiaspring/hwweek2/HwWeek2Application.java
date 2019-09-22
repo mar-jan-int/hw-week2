ec54bbde713241ed96accdab78ab6101bdbac023
mar-jan-int <mar.jan@int.pl>
2019-09-22 19:26:37 +0200

    init commit

---
+++ src/main/java/pl/akademiaspring/hwweek2/HwWeek2Application.java
@@ -0,0 +1,13 @@
+package pl.akademiaspring.hwweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwWeek2Application.class, args);
+	}
+
+}
